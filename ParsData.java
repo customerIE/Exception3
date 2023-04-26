b5305b4ab91e4cda85ddb001cc0df94729cbf0f9
Shapovalov Ilya <customerIE@mail.ru>
2023-04-26 10:05:05 +0300

    Seminar3

---
+++ ParsData.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+
+public class ParsData {
+
+    public HashMap<String, Object> parsInputData() {
+        InputData inputData = new InputData();
+        String[] data = inputData.enterData();
+        HashMap<String, Object> dataDic = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for (String i : data) {
+            if (i.length() == 1) {
+                if (i.equals("f") || i.equals("m")) {
+                    dataDic.put("sex", i);
+                } else{
+                    try {
+                        throw new GenderException();
+                    } catch (GenderException e) {
+                        e.genderException(i);
+                    }
+                }
+            } else if (i.matches("\\d{1,2}\\.\\d{1,2}\\.\\d{4}")) {
+                String[] arrayDate = i.split("\\.");
+                if (Integer.parseInt(arrayDate[0]) < 0 ||
+                        Integer.parseInt(arrayDate[0]) > 31 || Integer.parseInt(arrayDate[1]) < 0 ||
+                        Integer.parseInt(arrayDate[1]) > 12 || Integer.parseInt(arrayDate[2]) < 0 ||
+                        Integer.parseInt(arrayDate[2]) > 2022) {
+                    try {
+                        throw new DateException();
+                    } catch (DateException e) {
+                        e.dataException(i);
+                    }
+                } else {
+                    dataDic.put("date", i);
+                }
+
+            } else if (i.matches("[0-9]+")) {
+                dataDic.put("tel", i);
+            } else if (i.matches("[A-Za-z]+")) {
+                sb.append(i + " ");
+            } else {
+                try {
+                    throw new DataException();
+                } catch (DataException e) {
+                    e.dataException(i);
+                }
+            }
+        }
+        String[] fullName = String.valueOf(sb).split(" ");
+        if (fullName.length == 3) {
+            dataDic.put("firstName", fullName[1]);
+            dataDic.put("lastName", fullName[0]);
+            dataDic.put("patronymic", fullName[2]);
+        }
+        return dataDic;
+    }
+
+}
